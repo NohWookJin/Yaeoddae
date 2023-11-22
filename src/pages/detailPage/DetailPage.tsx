c48bc52698fe3bbdfc9df9bc0faa09b2c4098bbc
NohWookJin <jinn98@kakao.com>
2023-11-22 22:04:01 +0900

    Feat: 디테일 페이지 레이아웃 완료

--- src/pages/detailPage/DetailPage.tsx
+++ src/pages/detailPage/DetailPage.tsx
@@ -1,3 +1,25 @@
-export default function DetailPage() {
-  return <div>DetailPage</div>;
+import DetailSectionTop from "../../components/Detail/DetailSectionTop";
+import DetailDate from "../../components/Detail/DetailDate";
+import DetailSectionBottom from "../../components/Detail/DetailSectionBottom";
+
+import styled from "styled-components";
+
+function DetailPage() {
+  return (
+    <Container>
+      <DetailSectionTop />
+      <DetailDate />
+      <DetailSectionBottom />
+      <DetailSectionBottom />
+      <DetailSectionBottom />
+    </Container>
+  );
 }
+
+export default DetailPage;
+
+const Container = styled.section`
+  border-left:  ${({ theme }) => theme.Border.thinBorder};
+  border-right:  ${({ theme }) => theme.Border.thinBorder};
+  background-color: ${({ theme }) => theme.Color.backgroundColor}};
+`;
