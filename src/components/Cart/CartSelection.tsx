1e63edef1cc3e5ccdc7a63aa0d1f97f6499ac3ab
NohWookJin <jinn98@kakao.com>
2023-11-24 09:18:30 +0900

    Style: margin값 조정

--- src/components/Cart/CartSelection.tsx
+++ src/components/Cart/CartSelection.tsx
@@ -3,15 +3,10 @@ import styled from "styled-components";
 function CartSelection() {
   return (
     <Container>
-      <SectionTop>
-        <span>전체(2)</span>
-        <span>호텔(0)</span>
-        <span>모텔(0)</span>
-      </SectionTop>
-      <SectionBottom>
+      <Section>
         <input type="checkbox" />
         <span>전체선택(2/2)</span>
-      </SectionBottom>
+      </Section>
     </Container>
   );
 }
@@ -23,19 +18,7 @@ const Container = styled.div`
   background-color: ${({ theme }) => theme.Color.componentColor};
 `;
 
-const SectionTop = styled.div`
-  span {
-    margin-right: 0.5rem;
-    font-size: ${({ theme }) => theme.Fs.default};
-    color: ${({ theme }) => theme.Color.mainFontColor};
-    font-weight: 600;
-  }
-  padding-bottom: 0.5rem;
-  margin-bottom: 0.5rem;
-  border-bottom: ${({ theme }) => theme.Border.thinBorder};
-`;
-
-const SectionBottom = styled.div`
+const Section = styled.div`
   span {
     margin-left: 0.5rem;
     font-size: ${({ theme }) => theme.Fs.default};
