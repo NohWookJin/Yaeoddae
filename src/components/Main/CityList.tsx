f017fa0e22b86daf17a5f2c4934149170db40e24
김소정 <kimsojeong@gimsojeong-ui-iMac.local>
2023-11-28 12:17:42 +0900

    docs: mock 데이터 삭제

--- src/components/Main/CityList.tsx
+++ src/components/Main/CityList.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import CityItem from "./CityItem";
 import styled from "styled-components";
+import { API_BASE_URL } from "../../api/config";
 
 function CityList({ areacode }: CityListProp) {
   const [res, setRes] = useState<null | ItemType[]>(null);
@@ -9,9 +10,7 @@ function CityList({ areacode }: CityListProp) {
 
   useEffect(() => {
     axios
-      .get(
-        `https://travel-server.up.railway.app/accommodations/page/1?keyword=_&area-code=${areacode}`
-      )
+      .get(`${API_BASE_URL}/accommodations/page/1?keyword=_&area-code=${areacode}`)
       .then((response) => {
         const res = response.data.data;
         setRes(res.slice(0, 4));
@@ -41,39 +40,6 @@ const ListContainer = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
   overflow: hidden;
-`;
-
-const Skeleton = styled.div`
-  &.skeleton {
-    position: relative;
-    width: 150px;
-    height: 140px;
-  }
-  &::after {
-    content: "";
-    width: 150px;
-    height: 140px;
-    border-radius: ${({ theme }) => theme.Br.default};
-    color: rgba(0, 0, 0, 0);
-    background-image: linear-gradient(
-      100deg,
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0),
-      rgba(0, 0, 0, 0.1),
-      rgba(0, 0, 0, 0.1)
-    );
-    background-size: 400% 100%;
-    animation: skeleton-loading 7s linear infinite;
-    @keyframes skeleton-loading {
-      0% {
-        background-position: 200% 0;
-      }
-      100% {
-        background-position: -200% 0;
-      }
-    }
-  }
 `;
 
 export interface ItemType {
