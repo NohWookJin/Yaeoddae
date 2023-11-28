09c506e73024e037eedecc772e152d1044c80e08
김소정 <kimsojeong@gimsojeong-ui-iMac.local>
2023-11-29 00:18:40 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,24 @@ import HeaderRoute from "./routes/HeaderRoute";
 import MainStyleRoute from "./routes/MainStyleRoute";
 import SignupPage from "./pages/loginPage/SignupPage";
 import RoomDetailPage from "./pages/roomDetailPage/RoomDetailPage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route element={<MainStyleRoute />}>
-          <Route element={<HeaderRoute />}>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/" element={<MainPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/reservation" element={<ReservationPage />} />
-            <Route path="/detail/:id" element={<DetailPage />} />
-            <Route path="/room/:id" element={<RoomDetailPage />} />
-          </Route>
+    <Routes>
+      <Route element={<MainStyleRoute />}>
+        <Route element={<HeaderRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/reservation" element={<ReservationPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="/room/:id" element={<RoomDetailPage />} />
         </Route>
-      </Routes>
-    </QueryClientProvider>
+      </Route>
+    </Routes>
   );
 }
 
