9b1dc6f0418a2062716edcfaeb5a39e2be78d5fd
NohWookJin <jinn98@kakao.com>
2023-11-28 14:19:14 +0900

    Refactor: 실데이터 타입 변경

--- src/components/Detail/DetailSectionBottomBox.tsx
+++ src/components/Detail/DetailSectionBottomBox.tsx
@@ -2,21 +2,28 @@
 import DetailSectionBottom from "./DetailSectionBottom";
 import { RoomAccmodation } from "../../pages/detailPage/DetailPage";
 import { useCountStore } from "../../store/memberCount";
+import { useEffect, useState } from "react";
 
 interface AccommodationRooms {
-  accommodation: RoomAccmodation;
+  accommodation: RoomAccmodation[];
 }
 
 function DetailSectionBottomBox({ accommodation }: AccommodationRooms) {
+  const [rooms, setRooms] = useState<RoomAccmodation[]>([]);
+
+  useEffect(() => {
+    setRooms(accommodation);
+  }, [accommodation]);
+
   const member = useCountStore((state) => state.counts);
 
   return (
     <>
-      {accommodation.room.map((room) => {
+      {rooms.map((room) => {
         if (member > room.capacity) {
           return null;
         } else {
-          return <DetailSectionBottom key={room.id} room={room} />;
+          return <DetailSectionBottom key={room.roomTypeId} room={room} />;
         }
       })}
     </>
