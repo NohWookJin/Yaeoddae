e382a21ed2f2042a0d5100dc6918b7fad426b87c
seokmin <soekmin1998@naver.com>
2023-11-30 05:14:50 +0900

    Feat: 예약 내역 조회 페이지 레이아웃 제작

---
+++ src/components/Reservation/HistoryRoom.tsx
@@ -0,0 +1,101 @@
+// library
+import styled from "styled-components";
+// import { useState, useEffect } from "react";
+
+// component
+import { HistoryRoomProps } from "../../pages/reservationPage/ReservationHistoryPage";
+import dummy from "../../assets/dummy.png";
+
+function HistoryRoom({ room }: { room: HistoryRoomProps }) {
+  // const [roomInfo, setRoomInfo] = useState();
+  // const fetchRoomInfo = async () => {
+  //   const res = getData(`rooms/`)
+  // }
+
+  // useEffect(() => {
+  //   const res = await
+  // }, [room]);
+
+  return (
+    <>
+      <RoomDivider />
+      <HistoryRoomContainer>
+        <span>예약 번호 {room.id}</span>
+        <div>
+          <div>
+            <h2>{"스탠포드 호텔 서울"}</h2>
+            <a href="">숙소 상세 보기 &gt;</a>
+          </div>
+          <h3>{"더블 디럭스"}</h3>
+        </div>
+        <div>
+          <img src={dummy} alt="숙소 사진" />
+          <ReservationDescription>
+            <span>체크인: {room.check_in}</span>
+            <span>체크아웃: {room.check_out}</span>
+          </ReservationDescription>
+        </div>
+      </HistoryRoomContainer>
+    </>
+  );
+}
+
+export default HistoryRoom;
+
+const HistoryRoomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 1rem;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.Fs.mainFontColor};
+
+  span {
+    font-size: ${({ theme }) => theme.Fs.default};
+    color: #404040;
+    font-weight: 700;
+  }
+
+  > div:nth-child(2) {
+    div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+
+  h2 {
+    font-size: ${({ theme }) => theme.Fs.tagTitle};
+    font-weight: 700;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.Fs.default};
+  }
+
+  > div:last-child {
+    display: flex;
+    gap: 12px;
+
+    img {
+      width: 80px;
+      object-fit: cover;
+      border-radius: ${({ theme }) => theme.Br.default};
+    }
+  }
+`;
+
+const ReservationDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  span {
+    font-weight: 500;
+    font-size: ${({ theme }) => theme.Fs.default};
+  }
+`;
+
+const RoomDivider = styled.div`
+  width: 100%;
+  height: ${(props) => (props.$isLast ? "" : "0.125rem")};
+  background-color: ${({ theme }) => theme.Color.borderColor};
+`;
--- src/pages/reservationPage/ReservationHistoryPage.tsx
+++ src/pages/reservationPage/ReservationHistoryPage.tsx
@@ -3,18 +3,20 @@ import { useEffect, useState } from "react";
 import { getData } from "../../api/reservation";
 import ReservationHistoryItem from "../../components/Reservation/ReservationHistoryItem";
 
+export interface HistoryRoomProps {
+  id: number;
+  room_id: number;
+  check_in: string;
+  check_out: string;
+  guest_number: number;
+  status: string;
+}
+
 export interface ReservationHistory {
   id: number;
   created_at: string;
   paymentType: string;
-  reservation_rooms: {
-    id: number;
-    room_id: number;
-    check_in: string;
-    check_out: string;
-    guest_number: number;
-    status: string;
-  }[];
+  reservation_rooms: HistoryRoomProps[];
 }
 
 function ReservationHistoryPage() {
@@ -33,8 +35,8 @@ function ReservationHistoryPage() {
   }, []);
   return (
     <PageContainer>
-      {reservationHistory?.length !== 0 ? (
-        reservationHistory?.map((item, index) => {
+      {Dummy?.length !== 0 ? (
+        Dummy?.map((item, index) => {
           return <ReservationHistoryItem key={index} item={item} />;
         })
       ) : (
@@ -45,6 +47,47 @@ function ReservationHistoryPage() {
 }
 
 export default ReservationHistoryPage;
+
+const Dummy: ReservationHistory[] = [
+  {
+    id: 1,
+    created_at: "2023.11.29(수)",
+    paymentType: "KAKAO_PAY",
+    reservation_rooms: [
+      {
+        id: 4930294802894,
+        room_id: 1,
+        check_in: "2023.11.29(수)",
+        check_out: "2023.11.30(목)",
+        guest_number: 2,
+        status: "RESERVED",
+      },
+      {
+        id: 4930294802823,
+        room_id: 1,
+        check_in: "2023.11.29(수)",
+        check_out: "2023.11.30(금)",
+        guest_number: 1,
+        status: "RESERVED",
+      },
+    ],
+  },
+  {
+    id: 1,
+    created_at: "2023.11.30(수)",
+    paymentType: "CARD",
+    reservation_rooms: [
+      {
+        id: 4930294802894,
+        room_id: 1,
+        check_in: "2023.12.01(금)",
+        check_out: "2023.12.02(토)",
+        guest_number: 2,
+        status: "RESERVED",
+      },
+    ],
+  },
+];
 
 const PageContainer = styled.section`
   display: flex;
