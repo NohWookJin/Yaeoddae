580d88359541957ec4b929055857b5159f27b98d
NohWookJin <jinn98@kakao.com>
2023-11-30 04:08:55 +0900

    Fix: 상품 삭제시 전역 상태에 남아있던 문제 해결

---
+++ src/store/pickCartList.ts
@@ -0,0 +1,65 @@
+import { create } from "zustand";
+
+interface CartItem {
+  id: number;
+  price: number;
+}
+
+interface CartListState {
+  cart: CartItem[];
+  total: number;
+  addCartItem: (id: number, price: number, isChecked: boolean) => void;
+  removeCartItem: (id: number) => void;
+  resetCart: () => void;
+}
+
+export const pickCartList = create<CartListState>((set) => ({
+  cart: [],
+  total: 0,
+  addCartItem: (id, price, isChecked) => {
+    set((prevState) => {
+      const isItemInCart = prevState.cart.some((item) => item.id === id);
+
+      if (isItemInCart && !isChecked) {
+        const updatedCart = prevState.cart.filter((item) => item.id !== id);
+
+        return {
+          cart: updatedCart,
+          total: updatedCart.reduce((sum, item) => sum + item.price, 0),
+        };
+      } else if (isItemInCart) {
+        const updatedCart = prevState.cart.map((item) =>
+          item.id === id ? { ...item, price: price } : item
+        );
+
+        return {
+          cart: updatedCart,
+          total: updatedCart.reduce((sum, item) => sum + item.price, 0),
+        };
+      } else {
+        const updatedCart = [...prevState.cart, { id: id, price: price }];
+
+        return {
+          cart: updatedCart,
+          total: updatedCart.reduce((sum, item) => sum + item.price, 0),
+        };
+      }
+    });
+  },
+  removeCartItem: (id) => {
+    set((prevState) => {
+      const updatedCart = prevState.cart.filter((item) => item.id !== id);
+
+      return {
+        cart: updatedCart,
+        total: updatedCart.reduce((sum, item) => sum + item.price, 0),
+      };
+    });
+  },
+  resetCart: () => {
+    set({
+      cart: [],
+      total: 0,
+    });
+  },
+}));
