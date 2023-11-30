f6dd48892e20253eab25cb70c5b9252fdc1f8ffb
dbstjrals <soekmin1998@naver.com>
2023-12-01 01:02:56 +0900

    Fix: 예약 페이지 테스트 코드 변경

--- tests/reservationPage.spec.ts
+++ tests/reservationPage.spec.ts
@@ -11,37 +11,7 @@ const login = async ({ page }) => {
   await page.getByRole("button", { name: "로그인" }).click();
 };
 
-test("장바구니 상품 예약 시 예약페이지 내 상품 확인", async ({ page }) => {
-  login({ page });
-  await page
-    .locator("div")
-    .filter({ hasText: /^브라운스위트 신촌센트럴$/ })
-    .locator("div")
-    .click();
-  await page.goto(
-    "http://127.0.0.1:5173/detail/3023167?keyword=%EB%B8%8C%EB%9D%BC%EC%9A%B4%EC%8A%A4%EC%9C%84%ED%8A%B8%20%EC%8B%A0%EC%B4%8C%EC%84%BC%ED%8A%B8%EB%9F%B4&area-code=SEOUL&checkIn=231201&checkOut=231202&memberCount=2"
-  );
-  await page.getByRole("button", { name: "증가" }).click();
-  await page.locator(".cartButton").first().click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^브라운스위트 신촌센트럴서울특별시 마포구 서강로 137 \(노고산동\)$/ })
-    .getByRole("checkbox")
-    .check();
-  await page.getByRole("button", { name: "예약하기" }).click();
-
-  const expectData = {
-    accommodationName: page.getByRole("heading", { name: "브라운스위트 신촌센트럴" }),
-    roomName: page.getByRole("heading", { name: "디럭스 트리플" }),
-    guestNumber: page.getByText("기준 3명 / 최대 3명"),
-  };
-
-  await expect(expectData.accommodationName).toContainText("브라운스위트 신촌센트럴");
-  await expect(expectData.roomName).toContainText("디럭스 트리플");
-  await expect(expectData.guestNumber).toContainText("기준 3명");
-});
-
-test("단건 예약 확인", async ({ page }) => {
+test("예약 확인", async ({ page }) => {
   login({ page });
   await page
     .locator("div")
@@ -56,11 +26,13 @@ test("단건 예약 확인", async ({ page }) => {
     .first()
     .click();
   await page.getByRole("button", { name: "원 결제하기" }).click();
-  await page.goto("http://127.0.0.1:5173/reservation-history");
+  setTimeout(async () => {
+    await page.goto("http://127.0.0.1:5173/reservation-history");
+  }, 10000);
 
   const expectData = {
-    paymentType: page.getByText("예약 일자: 2023.11.30(목)결제 수단: 카카오페이"),
-    price: page.getByText("결제 금액: 110,000원"),
+    paymentType: page.getByText("결제 수단: 카카오페이").first(),
+    price: page.getByText("결제 금액: 110,000원").first(),
   };
 
   await expect(expectData.paymentType).toContainText("카카오페이");
